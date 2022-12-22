619b0cd0e79e0971f885095ad34e1cca0cb88acd
eemr3 <eemr3@yahoo.com.br>
2022-12-22 18:33:15 -0300

    criando a integração com o DATO CMS

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,21 +1,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { api } from '../../services/http';
+import { PorpsProject } from '../../helpers/interfaces';
 
-export default function Projects() {
-  const [projectData, setProjectData] = useState([]);
-
-  useEffect(() => {
-    const getProjects = async () => {
-      const response = await api.get('/projects.json');
-      console.log(response.data);
-
-      setProjectData(response.data);
-    };
-    getProjects();
-  }, []);
-
+export default function Projects({ data }: PorpsProject) {
   return (
     <section className="min-h-screen pb-24 bg-zinc-900 w-full" id="project">
       <div className="flex flex-col justify-center items-center">
@@ -25,17 +12,20 @@ export default function Projects() {
         </div>
         <div className="border-b-2 border-gray-50 w-52 my-4"></div>
       </div>
-      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 mt-7 gap-y-20 gap-x-8 lg:w-[90%] xl:w-8/12 m-auto">
-        {projectData?.map((item) => (
-          <div key={item.id} className="h-96 max-w-md m-auto">
+      <div
+        className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 mt-7
+       gap-y-20 gap-x-8 lg:w-[90%] xl:w-8/12 m-auto"
+      >
+        {data.map((item) => (
+          <div key={item.id} className="h-72 max-w-md m-auto">
             <Image
-              src={item.image}
+              src={item.imageProject.url}
               alt=""
-              className="object-cover h-96"
+              className="object-cover h-72"
               width={390}
               height={0}
             />
-            <Link href={item.url} target="_blank">
+            <Link href={item.urlProject} target="_blank">
               <p className="text-gray-900 text-xl text-center bg-gray-50 py-3 px-2 w-[390px] lg:w-auto">
                 <span className="text-orange-400">&lt;</span>
                 {item.title}
--- src/helpers/interfaces.ts
+++ src/helpers/interfaces.ts
@@ -12,3 +12,30 @@ export interface IProjectProps {
 export interface IDataProps {
   data: IDataProjectProps[];
 }
+
+export interface ImageProject {
+  id: string;
+  url: string;
+}
+
+export interface ProjectDataProps {
+  id: string;
+  imageProject: ImageProject;
+  title: string;
+  urlProject: string;
+}
+
+export interface PorpsProject {
+  data: ProjectDataProps[];
+}
+
+export interface AllProjectCards {
+  allProjectCards: ProjectDataProps;
+}
+
+export interface Data extends AllProjectCards {
+  content: {
+    allProjectCards: ProjectDataProps[];
+  };
+  projectData?: ProjectDataProps[];
+}
