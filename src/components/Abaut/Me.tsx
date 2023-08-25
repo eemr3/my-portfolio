89bb0a3efcc74de613b195d1bb7edee8c0f875db
Emerson Moreira <eemr3@yahoo.com.br>
2023-08-25 11:59:35 -0300

    refactor: about

--- src/components/Abaut/Me.tsx
+++ src/components/Abaut/Me.tsx
@@ -74,17 +74,10 @@ export default function Me() {
         }}
       >
         <p className="text-base text-justify text-gray-600 dark:text-white">
-          Desenvolvedor Web Full Stack Jr, sou apaixonado por tecnologia, principalmente
-          área de desenvolvimento, estou sempre disposto a encarar novos desafios e
-          aprender novas tecnologias, e culturas que engajam e impactam na vida de outras
-          pessoas, gosto de estar em ambientes colaborativos, com alto nível de
-          diversidade, onde podemos nos comunicar e nos desenvolver com outras pessoas que
-          também prezam por conexões humanas. Com muita vontade de contribuir através da
-          tecnologia para um mundo melhor, tenho foco em me tornar um desenvolvedor Full
-          Stack. Realizei projetos como estudante em minha trajetória na Trybe, utilizando
-          as tecnologias como: ReactJs, JavaScript, HTML5, CSS3, Bootstrap, GIT, Github,
-          Docker, NodeJs, TypeScript, MySQL e MongoDB, e atualmente estou atuando com dev
-          backend e tech lead em um projeto voluntário chamado Findy.
+          Olá! Sou um entusiasta do desenvolvimento web com paixão por explorar novas
+          tecnologias e metodologias para elevar a qualidade do meu código. Minha busca
+          contínua por código limpo e legível reflete meu comprometimento com a excelência
+          técnica.
         </p>
       </div>
     </div>
